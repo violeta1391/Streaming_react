c908b71673159aaba3a2b37d15330179b141069f
Violeta Rodriguez <violeta.rodriguez@globant.com>
2021-03-17 15:08:53 -0300

    footer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import Header from './Components/Header'
+import Footer from './Components/Footer'
 
 
 function App() {
   return (
     <div className="container-fluid" id="app">
       <Header />
+      <Footer />
 
     </div>
   );
--- my-app/src/Components/Header.jsx
+++ my-app/src/Components/Header.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <nav className="navbar justify-content-between container-fluid text-white bg-primary font-weight-bold">
-            <h2 className="font-weight-bold">DEMO Streaming</h2>
+        <nav className="navbar justify-content-between bg-primary">
+            <h2 className="font-weight-bold text-white">DEMO Streaming</h2>
             <form className="form-inline" id="navbar-form">
-                <button className="btn my-2 my-sm-0 btn_nav bg-primary text-white pr-2" type="submit">Log in   </button>
-                <button className="btn my-2 my-sm-0 bg-dark text-white" type="submit">Start your free trial</button>
+                <button className="btn btn_nav bg-primary text-white" type="submit">Log in</button>
+                <button className="btn bg-dark text-white" type="submit">Start your free trial</button>
             </form>
         </nav>
     );
