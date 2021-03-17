09fd63ba336285ae024bab580ae579223d44d49f
Violeta Rodriguez <violeta.rodriguez@globant.com>
2021-03-17 16:49:08 -0300

    sample json

--- my-app/src/Components/Movies.jsx
+++ my-app/src/Components/Movies.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import sample from '../Data/sample.json';
 
 
 const Movies = () => {
+
+
+    React.useEffect(() => {
+        document.title = `Movies`;
+      }, [])
+
+
     return (
         <div>
             <div className="bg-dark text-white popTitles">
--- my-app/src/Components/Series.jsx
+++ my-app/src/Components/Series.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
+import sample from '../Data/sample.json';
+
+const datos = sample.entries;
+let contador = 0;
 
 const Series = () => {
+
+    React.useEffect(() => {
+        document.title = `Series`;
+    }, [])
+
+    const tipo = datos.map(function (x) {
+        if (x.programType === "series" && x.releaseYear > 2010 && contador < 20) {
+            const seriesMap = {
+                titulos: x.title,
+                imagen: x.images
+            };
+            console.log(seriesMap)
+            contador++
+        }
+    })
+
+
+    const divStyle = {
+        color: 'blue'
+    };
+
     return (
         <div>
             <div className="bg-dark text-white popTitles">
                 <p>Popular Series</p>
             </div>
+            <div style={divStyle}>Hello World!</div>;
+            <ul className="lista">
+                {
+                    datos.map(item => (
+                        <li>
+                            <p>{item.title}</p>
+                        </li>
+                    ))
+                }
+            </ul>
         </div>
     );
 }
